154287768501f5b1f7a22152a270d5f755f341a7
Samuel Marks <samuelmarks@gmail.com>
2017-05-19 01:45:25 +1000

    Finished implementing `install_global_npm_packages1`; fixed bug in `install_node0`

--- offregister_node/ubuntu/__init__.py
+++ offregister_node/ubuntu/__init__.py
@@ -13,20 +13,20 @@ def install_node0(*args, **kwargs):
     apt_depends('curl', 'build-essential')
 
     run_cmd = partial(_run_command, sudo=kwargs.get('use_sudo', False))
-    run_cmd('mkdir -p Downloads')
     if exists('n'):
         return
+    run_cmd('mkdir -p Downloads')
     with cd('Downloads'):
         script = 'n-install.bash'
         if not exists(script):
-            run('curl -L https://git.io/n-install -o {}'.format(script))
+            run_cmd('curl -L https://git.io/n-install -o {}'.format(script))
         run_cmd('bash ./{script} -y \'{version}\''.format(script=script,
                                                           version=kwargs.get('node_version', 'latest')))
 
 
-def install_npm_package(*args, **kwargs):
+def install_global_npm_packages1(*args, **kwargs):
     if 'npm_global_packages' in kwargs:
-        if shell_env(PATH='$HOME/n/bin:$PATH'):
+        with shell_env(PATH='$HOME/n/bin:$PATH'):
             it_consumes(imap(lambda package: _run_command('npm install -g {package}'.format(package=package),
-                                                          sudo=kwargs.get('use_sudo', False))),
-                        kwargs['npm_global_packages'])
+                                                          sudo=kwargs.get('use_sudo', False)),
+                             kwargs['npm_global_packages']))
