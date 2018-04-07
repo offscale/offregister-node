793646e29e50f0070e299a1fe1d3e7d3f737143c
Samuel Marks <samuelmarks@gmail.com>
2018-04-07 15:05:17 +1000

    Better returns; now ensures the version specified is installed; overwrites previous n-install script; uses absolute paths

--- offregister_node/ubuntu/__init__.py
+++ offregister_node/ubuntu/__init__.py
@@ -1,31 +1,35 @@
 from functools import partial
 
 from fabric.contrib.files import exists
-from fabric.operations import run, _run_command
+from fabric.operations import _run_command
 from fabric.context_managers import cd, shell_env
 
 from offregister_fab_utils.apt import apt_depends
+from offregister_fab_utils.fs import cmd_avail
 
 
-def install_node0(*args, **kwargs):
+def install_node0(node_version='latest', *args, **kwargs):
     apt_depends('curl', 'build-essential')
 
     run_cmd = partial(_run_command, sudo=kwargs.get('node_sudo', kwargs.get('use_sudo', False)))
-    if exists('n'):
-        return
-    run_cmd('mkdir -p Downloads')
-    with cd('Downloads'):
-        script = 'n-install.bash'
-        if not exists(script):
+    if cmd_avail('n') and not exists('$HOME/n/n/versions/node/{version}'.format(version=node_version)):
+        run_cmd('n {version}'.format(version=node_version))
+    else:
+        run_cmd('mkdir -p $HOME/Downloads')
+        with cd('$HOME/Downloads'):
+            script = 'n-install.bash'
             run_cmd('curl -L https://git.io/n-install -o {}'.format(script))
-        run_cmd('bash ./{script} -y \'{version}\''.format(script=script,
-                                                          version=kwargs.get('node_version', 'latest')))
+            run_cmd('bash ./{script} -y \'{version}\''.format(script=script,
+                                                              version=node_version))
+
+    return {'Node.js version installed': node_version}
 
 
 def install_global_npm_packages1(*args, **kwargs):
     if 'npm_global_packages' in kwargs:
         with shell_env(PATH='$HOME/n/bin:$PATH'):
-            print "install_global_npm_packages1::kwargs.get('use_sudo', False) =", kwargs.get('use_sudo', False)
-            _run_command('npm install -g {packages}'.format(packages=' '.join(kwargs['npm_global_packages']),
+            packages = ' '.join(kwargs['npm_global_packages'])
+            _run_command('npm install -g {packages}'.format(packages=packages,
                                                             sudo=kwargs.get('node_sudo',
                                                                             kwargs.get('use_sudo', False))))
+            return {'Node.js globally installed': packages}
