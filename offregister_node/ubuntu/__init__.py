70019562c972848b1f52cf7245e335bc2c89dff7
Samuel Marks <samuelmarks@gmail.com>
2018-04-07 15:39:01 +1000

    Moved Downloads directory dance; added N_PREFIX support

--- offregister_node/ubuntu/__init__.py
+++ offregister_node/ubuntu/__init__.py
@@ -12,22 +12,22 @@ def install_node0(node_version='latest', *args, **kwargs):
     apt_depends('curl', 'build-essential')
 
     run_cmd = partial(_run_command, sudo=kwargs.get('node_sudo', kwargs.get('use_sudo', False)))
-    if cmd_avail('n') and not exists('$HOME/n/n/versions/node/{version}'.format(version=node_version)):
+    n_prefix = kwargs.get('N_PREFIX', run_cmd('echo $HOME/n', quiet=True))
+    if cmd_avail('n') and not exists('{n_prefix}/n/versions/node/{version}'.format(n_prefix=n_prefix,
+                                                                                   version=node_version)):
         run_cmd('n {version}'.format(version=node_version))
     else:
-        run_cmd('mkdir -p $HOME/Downloads')
-        with cd('$HOME/Downloads'):
-            script = 'n-install.bash'
-            run_cmd('curl -L https://git.io/n-install -o {}'.format(script))
-            run_cmd('bash ./{script} -y \'{version}\''.format(script=script,
-                                                              version=node_version))
+        run_cmd("curl -L https://git.io/n-install"
+                " | N_PREFIX='{n_prefix}' bash -s -- -y '{version}'".format(n_prefix=n_prefix, version=node_version))
 
     return {'Node.js version installed': node_version}
 
 
 def install_global_npm_packages1(*args, **kwargs):
+    n_prefix = kwargs.get('N_PREFIX', '$HOME/n')
+
     if 'npm_global_packages' in kwargs:
-        with shell_env(PATH='$HOME/n/bin:$PATH'):
+        with shell_env(PATH='{n_prefix}/bin:$PATH'.format(n_prefix=n_prefix)):
             packages = ' '.join(kwargs['npm_global_packages'])
             _run_command('npm install -g {packages}'.format(packages=packages,
                                                             sudo=kwargs.get('node_sudo',
