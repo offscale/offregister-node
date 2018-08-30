c97e94d189d31de64457a8329040d30cf972895a
Samuel Marks <samuelmarks@gmail.com>
2018-08-30 12:41:17 +1000

    Bifurcated into Ubuntu specific and *nix agnostic utils

--- offregister_node/ubuntu/__init__.py
+++ offregister_node/ubuntu/__init__.py
@@ -1,35 +1,13 @@
-from functools import partial
-
-from fabric.contrib.files import exists
-from fabric.operations import _run_command
-from fabric.context_managers import cd, shell_env
-
 from offregister_fab_utils.apt import apt_depends
-from offregister_fab_utils.fs import cmd_avail
+
+from offregister_node.utils import install_node, install_global_npm_packages
 
 
 def install_node0(node_version='latest', *args, **kwargs):
     apt_depends('curl', 'build-essential')
 
-    run_cmd = partial(_run_command, sudo=kwargs.get('node_sudo', kwargs.get('use_sudo', False)))
-    n_prefix = kwargs.get('N_PREFIX', run_cmd('echo $HOME/n', quiet=True))
-    if cmd_avail('n') and not exists('{n_prefix}/n/versions/node/{version}'.format(n_prefix=n_prefix,
-                                                                                   version=node_version)):
-        run_cmd('n {version}'.format(version=node_version))
-    else:
-        run_cmd("curl -L https://git.io/n-install"
-                " | N_PREFIX='{n_prefix}' bash -s -- -y '{version}'".format(n_prefix=n_prefix, version=node_version))
-
-    return {'Node.js version installed': node_version}
+    return install_node(node_version=node_version, **kwargs)
 
 
 def install_global_npm_packages1(*args, **kwargs):
-    n_prefix = kwargs.get('N_PREFIX', '$HOME/n')
-
-    if 'npm_global_packages' in kwargs:
-        with shell_env(PATH='{n_prefix}/bin:$PATH'.format(n_prefix=n_prefix)):
-            packages = ' '.join(kwargs['npm_global_packages'])
-            _run_command('npm install -g {packages}'.format(packages=packages,
-                                                            sudo=kwargs.get('node_sudo',
-                                                                            kwargs.get('use_sudo', False))))
-            return {'Node.js globally installed': packages}
+    return install_global_npm_packages(**kwargs)
---
+++ offregister_node/utils.py
@@ -0,0 +1,31 @@
+from functools import partial
+
+from fabric.context_managers import shell_env
+from fabric.contrib.files import exists
+from fabric.operations import _run_command
+from offregister_fab_utils.fs import cmd_avail
+
+
+def install_node(node_version='latest', *args, **kwargs):
+    run_cmd = partial(_run_command, sudo=kwargs.get('node_sudo', kwargs.get('use_sudo', False)))
+    n_prefix = kwargs.get('N_PREFIX', run_cmd('echo $HOME/n', quiet=True))
+    if cmd_avail('n') and not exists('{n_prefix}/n/versions/node/{version}'.format(n_prefix=n_prefix,
+                                                                                   version=node_version)):
+        run_cmd('n {version}'.format(version=node_version))
+    else:
+        run_cmd("curl -L https://git.io/n-install"
+                " | N_PREFIX='{n_prefix}' bash -s -- -y '{version}'".format(n_prefix=n_prefix, version=node_version))
+
+    return {'Node.js version installed': node_version}
+
+
+def install_global_npm_packages(*args, **kwargs):
+    n_prefix = kwargs.get('N_PREFIX', '$HOME/n')
+
+    if 'npm_global_packages' in kwargs:
+        with shell_env(PATH='{n_prefix}/bin:$PATH'.format(n_prefix=n_prefix)):
+            packages = ' '.join(kwargs['npm_global_packages'])
+            _run_command('npm install -g {packages}'.format(packages=packages,
+                                                            sudo=kwargs.get('node_sudo',
+                                                                            kwargs.get('use_sudo', False))))
+            return {'Node.js globally installed': packages}
