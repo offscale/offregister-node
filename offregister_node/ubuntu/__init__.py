af43e0fb14ca7809c5a1c438e21d3051185e6876
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-09-15 15:11:03 +1000

    [README.md] New badges; [LICENSE-{APACHE,MIT}] Update copyright year range; [*.py] Python 3 support; Blacken; [requirements.txt,setup.py] Fix dependencies to reflect new names / Python 3

--- offregister_node/ubuntu/__init__.py
+++ offregister_node/ubuntu/__init__.py
@@ -3,8 +3,8 @@ from offregister_fab_utils.apt import apt_depends
 from offregister_node.utils import install_node, install_global_npm_packages
 
 
-def install_node0(node_version='latest', *args, **kwargs):
-    apt_depends('curl', 'build-essential')
+def install_node0(node_version="latest", *args, **kwargs):
+    apt_depends("curl", "build-essential")
 
     return install_node(node_version=node_version, **kwargs)
 
